f881a1998bcd8867d5ad7467344ce56b6d9e42eb
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2024-03-21 14:55:45 -0300

    Ajeitando ordem de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
             />
-            <ProjectInfo info={info}></ProjectInfo>
             <PersonalIntroduction></PersonalIntroduction>
+            <ProjectInfo info={info}></ProjectInfo>
         </>
     );
 }
